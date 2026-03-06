b371ecefb165c49d0ff3e35fd9675f7fca1830ee
Gantakuri Veera Venkata Sai <sai.ganthakuri@gmail.com>
2026-03-06 14:19:03 +0530

    Delete dataset directory

--- dataset/Customer_Ranking.sql
+++
@@ -1,7 +0,0 @@
--- customer ranking
-SELECT
-customer_id,
-customer_name,
-total_spend,
-RANK() OVER (ORDER BY total_spend DESC) AS revenue_rank
-FROM customer_analysis;
--- dataset/Revenue.sql
+++
@@ -1,8 +0,0 @@
--- revenue contribution
-SELECT
-customer_id,
-total_spend,
-SUM(total_spend) OVER() AS total_revenue,
-total_spend / SUM(total_spend) OVER() AS revenue_contribution
-FROM customer_metrics;
-show tables;
